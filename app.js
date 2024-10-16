e8927116de5e80242502497e6382604f6b7aed9a
Luciano Baconski <lbaconski@gmail.com>
2024-10-16 16:09:16 -0300

    DB Connection. Campground model

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', )
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("DB OK");
+})
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
